79d223d00d2f616e20807ccd8d5877b8e4245596
GYURI <GYURI@192.168.123.111>
2022-05-23 18:18:17 +0900

    프로그램 소스를 GitHub에 올립니다.

---
+++ TEST/src/Hello.java
@@ -0,0 +1,15 @@
+
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("안녕하세요. 자바World에 오신 걸 환영합니다.");
+		System.out.println("제 이름은 최규리입니다.");
+		// Ctrl + F11 = 실행
+		// 주석 : 주석은 프로그램 실행과는 상관이 없다.
+		/*
+		 *  여러줄 주석
+		 *  */
+	}
+
+}
